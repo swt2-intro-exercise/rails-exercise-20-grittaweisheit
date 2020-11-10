478945694599e5d0afa28a548953057348b54664
Gritta Weisheit <grittaweisheit@gmail.com>
2020-11-10 17:59:57 +0100

    add author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,15 @@
 class AuthorsController < ApplicationController
+  def new
+  end
 
-    def new
+  def create
+    @author = Author.new(author_params)
+    @author.save
+    redirect_to root_path
+  end
+
+  private
+    def author_params
+      params.require(:author).permit(:first_name, :last_name, :homepage)
     end
 end
