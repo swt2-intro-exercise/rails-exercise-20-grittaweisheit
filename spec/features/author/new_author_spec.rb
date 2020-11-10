7ef6ab82ece07e584dd7d5a47be1fb3274b09f2e
Gritta Weisheit <grittaweisheit@gmail.com>
2020-11-10 15:48:01 +0100

    new author input test

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 describe "New author page", type: :feature do
   
-  it "should render withour error" do
+  it "should render without error" do
     visit new_author_path
+  end
+
+  it "should have text inputs for an author's first name, last name, and homepage" do
+    visit new_author_path
+    
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
   end
 end
